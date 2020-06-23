ef5fce65e27c8722e0087f507b7fd35500789ed6
Kunal Pant <kunalpant@Kunals-MacBook-Air.local>
2020-06-23 20:54:16 +0530

    This is Pokemon

---
+++ src/component/pokemon/Pokemon.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+//import data from '../../data/pokemons.json'
+
+const Pokemon = (props) => {
+  const details = props.details;
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img src={details.image} />
+
+        <Card.Body>
+          <Card.Title> {details.name}</Card.Title>
+          <Card.Text>{details.type.map(item => `${item} `)}</Card.Text>
+          <Card.Text>{Object.entries(details.base).map(item => <p>{`${item[0]} - ${item[1]}`}</p>)}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Pokemon;
